8fdcf61c3c98aa694dbcfa38382c56b6bc6d1854
dimka90 <dimkayilrit@gmail.com>
2025-08-09 23:16:33 +0100

    custom_meta_data: implementated custom logic for extracting file info and details

--- src/dim/manifest.rs
+++ src/dim/manifest.rs
@@ -1,15 +1,75 @@
+use serde::de::value::Error;
 use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read};
+use std::path::Path;
+use anyhow::Result;
+use std::vec;
 
-#[derive(Deserialize, Serialize)]
-pub struct  ChunckInfo{
+use crate::util::hash::keccak_256;
+
+const CHUNK_SIZE:usize  = 256 * 1024;
+#[derive(Deserialize, Serialize, Debug)]
+pub struct  ChunkInfo{
     pub index: usize,
     pub hash: Vec<u8>,
     pub size: usize
 }
 
-impl  ChunckInfo {
+impl  ChunkInfo {
    pub  fn hash_hex(&self) -> String{
         hex::encode(&self.hash)
 
     }
+}
+
+#[derive(Deserialize,Serialize, Debug)]
+pub struct  DimManifest{
+    pub file_name: String,
+    pub file_size: u64,
+    pub chunk_size: usize,
+    pub chunks: Vec<ChunkInfo>,
+}
+
+pub fn  create_manifest<P: AsRef<Path>>(file_path: P)  -> Result<DimManifest>{
+let file = File::open(&file_path)?;
+let meta_data = file.metadata()?;
+let file_size = meta_data.len();
+let file_name = &file_path.as_ref().file_name()
+                                 .and_then(|os_str| os_str.to_str())
+                                 .unwrap_or("Unknown ")
+                                 .to_string();
+
+let mut reader = BufReader::new(file);
+let mut chunks = Vec::new();
+let mut  index = 0;
+
+loop {
+
+    let mut buffer = vec![0u8; CHUNK_SIZE];
+    let byte_read  = reader.read(&mut buffer)?;
+
+    if byte_read == 0{
+        break;
+    }
+    buffer.truncate(byte_read);
+    let hash_bytes = keccak_256(&buffer);;
+    chunks.push(ChunkInfo{
+        index,
+        hash:hash_bytes.to_vec(),
+        size: byte_read
+
+    });
+    index +=1;
+}
+
+
+println!("File {:?} \n Size:{:?} {:?} " , meta_data,file_size, file_name);
+
+Ok(DimManifest{
+    file_name: file_name.clone(),
+    file_size,
+    chunk_size: CHUNK_SIZE,
+    chunks
+})
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod util;
 mod dim;
+
 use std::env;
 use  util::hash::keccak_256;
+use  dim::manifest::create_manifest;
  fn main() {
 
     let input_file = env::args().nth(1).expect("Expect a file ");
 
-    println!("{:?}", input_file);
-    let numbers = vec![1, 2, 4, ];
-    println!("Hash: {:?}", keccak_256(&numbers));
-    println!("Hello, world!");
+    let chuncks = create_manifest(input_file);
+    println!("{:#?}", chuncks);
 }
